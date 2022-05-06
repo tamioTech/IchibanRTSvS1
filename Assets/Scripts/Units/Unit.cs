014347eb6c30b23a642c43ef8ff1f33f8635bd13
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-05-05 17:05:21 -0700

    adding unit selection handler

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Mirror;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnityEvent onSelected;
+    [SerializeField] private UnityEvent onDeselected;
+
+    #region Client
+
+    [Client]
+    public void Select()
+    {
+        if (!hasAuthority) { return; }
+
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!hasAuthority) { return; }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
+}
