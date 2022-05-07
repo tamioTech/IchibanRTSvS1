7597962d2df2ff81b8d73996a5b8466d46c802e3
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-05-06 22:29:26 -0700

    selecting and moving unit working

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -10,7 +10,7 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private Camera mainCamera;
 
-    private List<Unit> selectedUnits = new List<Unit>();
+    public List<Unit> SelectedUnits { get; } = new List<Unit>();
 
     private void Start()
     {
@@ -22,12 +22,12 @@ public class UnitSelectionHandler : MonoBehaviour
     {
         if(Mouse.current.leftButton.wasPressedThisFrame)
         {
-            foreach(Unit selectedUnit in selectedUnits)
+            foreach(Unit selectedUnit in SelectedUnits)
             {
                 selectedUnit.Deselect();
             }
 
-            selectedUnits.Clear();
+            SelectedUnits.Clear();
         }
         else if(Mouse.current.leftButton.wasReleasedThisFrame)
         {
@@ -45,9 +45,9 @@ public class UnitSelectionHandler : MonoBehaviour
 
         if (!unit.hasAuthority) { return; }
 
-        selectedUnits.Add(unit);
+        SelectedUnits.Add(unit);
 
-        foreach(Unit selectedUnit in selectedUnits)
+        foreach(Unit selectedUnit in SelectedUnits)
         {
             selectedUnit.Select();
         }
